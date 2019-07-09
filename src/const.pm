c036ce4b72051f1631aa7c84575eec874fdc32cd
mle <maximilian@eul.cc>
2019-07-09 18:10:50 +0200

    cleanup: extract ansi constants into module

---
+++ src/const.pm
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+use strict;
+use vars qw(
+	$c_bold $c_unbold
+	$c_faint $c_unfaint
+	$c0
+);
+
+
+## ANSI constants:  ############################################################
+
+our $c_bold   = '[1m';
+our $c_unbold = '[22m';
+
+our $c_faint   = '[2m';
+our $c_unfaint = '[22m';
+
+our $c0 = '[0m';
+
+
+1
