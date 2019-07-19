6bb30534f01ae23f407799003a529d7fb4263185
mle <maximilian@eul.cc>
2019-07-19 17:29:18 +0200

    cleanup: extract color constants into module

---
+++ src/Color.pm
@@ -0,0 +1,65 @@
+#!/usr/bin/perl
+use strict;
+use vars qw(
+	$c_bold $c_unbold
+	$c_faint $c_unfaint
+	$c0
+	$c_sym $c_contsym $c_meta $c_followsep
+	$c_rpt
+	$c_date $c_host $c_app
+	$c_loglevel $c_loglevel_warn $c_loglevel_err
+	$c_info $c_info_prefix
+	$c_trace $c_stack $c_stack_msg
+	$c_exception_fqcn $c_exception_scn $c_exception_code $c_exception0
+	$c_message
+	$c_http_success $c_http_redir $c_http_client_error $c_http_client_failure $c_http_server_error
+);
+
+
+## Base constants:  ############################################################
+
+our $c_bold   = '[1m';
+our $c_unbold = '[22m';
+
+our $c_faint   = '[2m';
+our $c_unfaint = '[22m';
+
+our $c0 = '[0m';
+
+
+
+## Color constants:  ###########################################################
+
+# Base color configuration:
+our $c_sym = '[33m';
+our $c_contsym = '[38;2;113;97;25m';
+our $c_meta = '[38;2;114;204;204m';
+our $c_followsep = '[32m'.$c_faint;
+
+# Detailed color configuration:
+our $c_date = $c_sym;
+our $c_host = $c_sym;
+our $c_app = $c_sym;
+our $c_loglevel = $c_sym;
+our $c_loglevel_warn = '[38;5;220m';
+our $c_loglevel_err = '[38;2;255;145;36m';
+our $c_info = '[38;5;243m';
+our $c_info_prefix = '[38;2;125;117;83m';
+our $c_trace = $c_info;
+our $c_stack = $c_info;
+our $c_stack_msg = $c0;
+our $c_rpt = '[34m';
+our $c_exception_fqcn = $c_unbold;
+our $c_exception_scn  = $c_bold;
+our $c_exception_code = $c_bold;
+our $c_exception0 = $c_unbold;
+our $c_message = $c0;
+
+our $c_http_success        = '[38;2;98;214;113m';
+our $c_http_redir          = '[38;2;202;214;98m';
+our $c_http_client_error   = '[38;2;155;72;72m';
+our $c_http_client_failure = '[38;2;235;41;41m';
+our $c_http_server_error   = '[38;5;199;1m';
+
+
+1
