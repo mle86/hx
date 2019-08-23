3a43896f947319debc55011d9e9a6b1e80cfd4f3
mle <maximilian@eul.cc>
2019-08-23 19:01:21 +0200

    test: add basic color checks

---
+++ test/02-test-basic-colors.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+. $(dirname "$0")/init.sh
+
+
+SYSLOGLINE="$(head -n1 -- "$SYSLOG" | "$HX")"
+KERNELLINE="$(grep -m1 'OUT=eth0' -- "$SYSLOG" | "$HX")"
+REPEATLINE="$(grep -m1 'repeated 5 times' -- "$SYSLOG" | "$HX")"
+
+color_date='33'
+color_host="$color_date"
+color_app="$color_date"
+color_msg='37|0'
+color_rpt='34'
+color_info='30|37|38;5;243'
+color_prefix="$color_info|38;2;125;117;83"
+
+
+assertRegex "$SYSLOGLINE" "/$(re_col $color_date)Jun 16\\b/"
+assertRegex "$SYSLOGLINE" "/$(re_col $color_host)test-pc\\b/"
+assertRegex "$SYSLOGLINE" "/$(re_col $color_app)rsyncd\\b/"
+assertRegex "$SYSLOGLINE" "/$(re_col $color_msg)sent 63 bytes\\b/"
+
+assertRegex "$KERNELLINE" "/$(re_col $color_prefix)\\[269611.241825\\]/"
+
+assertRegex "$REPEATLINE" "/$(re_col $color_rpt)message repeated 5 times:/"
+
+
+success
--- test/config.sh
+++ test/config.sh
@@ -2,3 +2,20 @@
 
 HX="$HERE/../src/hx"
 
+SYSLOG="$HERE/samples/syslog.log"
+
+
+# re_b
+#  Regex to match the sequence which turns text bold.
+re_b () { printf '%s' "\\x1b\\[(?:(?:\\d\\+;)*0;)?1m"; }
+
+# re_col COLORCODE
+#  Regex to match the sequence which colorizes text.
+#  Separate multiple allowed color codes with "|".
+re_col () { printf '%s' "\\x1b\\[(?:(?:\\d\\+;)*0;)?(?:$1)m"; }
+
+# re_b_col COLORCODE
+#  Regex to match the sequence which colorizes and boldens text.
+#  Separate multiple allowed color codes with "|".
+re_b_col () { printf '%s' "\\x1b\\[(?:(?:\\d\\+;)*0;)?(?:(?:$1)(?:;|\\x1b\\[)1|1(?:;|\\x1b\\[)(?:$1))m"; }
+
