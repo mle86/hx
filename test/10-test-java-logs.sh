f7077026b2344ec08cc50cd0e1103d5c5bc81d06
mle <maximilian@eul.cc>
2020-04-24 13:32:52 +0200

    recognize extra spaces after loglevel
    Java logs sometimes use spaces after loglevel to align classnames

--- test/10-test-java-logs.sh
+++ test/10-test-java-logs.sh
@@ -8,7 +8,7 @@ logfile="$HERE/samples/java.log"
 # 2019-10-24 14:31:18.484 FINE MyUtil: moved /tmp/file1 to /tmp/file2 [info]
 line="$(logline "$logfile" 1 | LEX)"
 assertRegex "$line" "/$(re_tok $T_DATE "2019-10-24 14:31:18\.484")/"
-assertRegex "$line" "/$(re_tok $T_LOGLEVEL "FINE")/"
+assertRegex "$line" "/$(re_tok $T_LOGLEVEL "FINE ?")/"
 assertRegex "$line" "/$(re_tok $T_APP "MyUtil:?")/"
 assertRegex "$line" "/$(re_tok $T_MESSAGE ":? ?moved.*")/"
 
