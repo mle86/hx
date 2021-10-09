d1ec6ad173836a8bd98ceabe689c7f44a1bf5258
mle <maximilian@eul.cc>
2021-10-09 14:48:47 +0200

    recognize kernel continuation lines

--- test/11-test-kernel-logs.sh
+++ test/11-test-kernel-logs.sh
@@ -13,5 +13,9 @@ reApp="(?:$(re_tok $T_APP "kernel:")$(re_tok $T_APP "rcu:")|$(re_tok $T_APP "ker
 reMsg="$(re_tok $T_MESSAGE "Hierarchical.*")"
 assertRegex "$line" "/$reDate$reApp$reMsg/"
 
+# [    0.247623] kernel:   #2  #3  #4  #5  #6  #7  #8  #9 #10 #11 #12 #13 #14 #15
+line="$(logline "$logfile" 2 | LEX)"
+assertRegex "$line" "/$(re_tok $T_CONTLINE).*$(re_tok $T_INFO " *#2  #3.*")/"
+
 
 success
